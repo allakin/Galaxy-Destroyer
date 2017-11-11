04641b6bd68ce74f5d30e510ac27041e116e3778
Roman Bakhilov <roman.bakhilov@gmail.com>
2017-11-11 17:46:03 +0200

    Implemented enemy spawning Changed fire logic

--- Galaxy Destroyer/GameScene.swift
+++ Galaxy Destroyer/GameScene.swift
@@ -13,7 +13,29 @@ class GameScene: SKScene {
     let player = SKSpriteNode(imageNamed: "playerShip")
     
     let bulletSound = SKAction.playSoundFileNamed("torpedo", waitForCompletion: false)
-
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(min min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    var gameArea: CGRect
+    override init(size: CGSize) {
+        let maxAspectRatio: CGFloat = 16.0/9.0
+        let playableWidth = size.height / maxAspectRatio
+        let margin = (size.width - playableWidth) / 2
+        gameArea = CGRect(x: margin, y: 0, width: playableWidth, height: size.height)
+        
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.size = self.size
@@ -25,6 +47,25 @@ class GameScene: SKScene {
         player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.1)
         player.zPosition = 2
         self.addChild(player)
+        startNewLevel()
+        spawnFire()
+    }
+    
+    func startNewLevel() {
+        let spawn = SKAction.run(spawnEnemy)
+        let spawnFire = SKAction.run(fireBullet)
+        let waitToSpawn = SKAction.wait(forDuration: 1)
+        let spawnSequence = SKAction.sequence([spawn, spawnFire, waitToSpawn])
+        let spawnForever = SKAction.repeatForever(spawnSequence)
+        self.run(spawnForever)
+    }
+    
+    func spawnFire() {
+        let spawnFire = SKAction.run(fireBullet)
+        let waitToSpawnFire = SKAction.wait(forDuration: 0.2)
+        let spawnSequenceFire = SKAction.sequence([spawnFire, waitToSpawnFire])
+        let spawnForeverFire = SKAction.repeatForever(spawnSequenceFire)
+        self.run(spawnForeverFire)
     }
     
     func fireBullet() {
@@ -42,9 +83,32 @@ class GameScene: SKScene {
         
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        fireBullet()
+    func spawnEnemy() {
+        let randomXStart = random(min: gameArea.minX, max: gameArea.maxX)
+        let randomXEnd = random(min: gameArea.minX, max: gameArea.maxX)
+        
+        let startPoint = CGPoint(x: randomXStart, y: self.size.height * 1.2)
+        let endPoint = CGPoint(x: randomXEnd, y: -self.size.height * 0.2)
+        let enemy = SKSpriteNode(imageNamed: "enemyShip")
+        enemy.setScale(1.5)
+        enemy.position = startPoint
+        enemy.zPosition = 2
+        self.addChild(enemy)
+        
+        let moveEnemy = SKAction.move(to: endPoint, duration: 5)
+        let deleteEnemy = SKAction.removeFromParent()
+        let enemySequence = SKAction.sequence([moveEnemy, deleteEnemy])
+        enemy.run(enemySequence)
+        
+        let dx = endPoint.x - startPoint.x
+        let dy = endPoint.y - startPoint.y
+        let amountToRotate = atan2(dy, dx)
+        enemy.zRotation = amountToRotate
     }
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        fireBullet()
+//    }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch: AnyObject in touches {
@@ -52,6 +116,13 @@ class GameScene: SKScene {
             let previousPointOfTouch = touch.previousLocation(in: self)
             let amountDragged = pointOfTouch.x - previousPointOfTouch.x
             player.position.x += amountDragged
+            
+            if player.position.x > gameArea.maxX - player.size.width/2 {
+                player.position.x = gameArea.maxX - player.size.width/2
+            }
+            if player.position.x < gameArea.minX + player.size.width/2 {
+                player.position.x = gameArea.minX + player.size.width/2
+            }
         }
     }
     
